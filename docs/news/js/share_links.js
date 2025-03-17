0a9603d266c871ce099ca4967558e8fc7e1282d8
xdberiuz <xdvooddoo@gmail.com>
2025-03-17 16:42:01 +0200

    new social links

--- docs/news/js/share_links.js
+++ docs/news/js/share_links.js
@@ -26,56 +26,58 @@ menuBtn.addEventListener("click", () => {
 
 //share button on page News
 
-var URL_SHAER_FACEBOOK = 'https://www.facebook.com/sharer/sharer.php?u=';
-var URL_SHARE_TELEGRAM = 'https://telegram.me/share/url?url=';
+var URL_SHAER_FACEBOOK = 'https://www.facebook.com/art.office.com.ua/';
+var URL_SHARE_TELEGRAM = 'https://t.me/art_office';
 var correctLink = window.location.href.substring(0, window.location.href.length - 5);
 
 //share link
 function shareBtnLink(obj, link) {
-    let nextPageLink = link;
-    navigator.clipboard.writeText(correctLink + `/${nextPageLink}`);
-    var checkmark = document.getElementById('checkmark');
-      checkmark.style.display = 'inline-block';
-      setTimeout(function() {
-        checkmark.style.display = 'none';
-      }, 3000);
+  let nextPageLink = link;
+  navigator.clipboard.writeText(correctLink + `/${nextPageLink}`);
+  var checkmark = document.getElementById('checkmark');
+  checkmark.style.display = 'inline-block';
+  setTimeout(function () {
+    checkmark.style.display = 'none';
+  }, 3000);
 }
 
 //share FB
 
 function shareBtnFb(linkFb) {
-    let fullLinkFb = URL_SHAER_FACEBOOK + correctLink + `/${linkFb}`
-    window.open(fullLinkFb, '_blank').focus();
+  // let fullLinkFb = URL_SHAER_FACEBOOK + correctLink + `/${linkFb}`
+  let fullLinkFb = URL_SHAER_FACEBOOK;
+  window.open(fullLinkFb, '_blank').focus();
 }
 
 //share Telegram
 
 function shareBtnTm(linkTm) {
-    let fullLinkTm = URL_SHARE_TELEGRAM + correctLink + `/${linkTm}`
-    window.open(fullLinkTm, '_blank').focus();
+  // let fullLinkTm = URL_SHARE_TELEGRAM + correctLink + `/${linkTm}`
+  let fullLinkTm = URL_SHARE_TELEGRAM;
+  window.open(fullLinkTm, '_blank').focus();
 }
 
 //share button on news content page
 
 $('.url').click(function () {
-    navigator.clipboard.writeText(window.location.href)
-    var checkmark = document.getElementById('checkmark');
-    checkmark.style.display = 'inline-block';
-    setTimeout(function() {
-      checkmark.style.display = 'none';
-    }, 3000);
+  navigator.clipboard.writeText(window.location.href)
+  var checkmark = document.getElementById('checkmark');
+  checkmark.style.display = 'inline-block';
+  setTimeout(function () {
+    checkmark.style.display = 'none';
+  }, 3000);
 });
 
 //share Telegram
 
 $('.share_social .tm').click(function () {
-    let fullLink = URL_SHARE_TELEGRAM + window.location.href
-    window.open(fullLink, '_blank').focus();
+  let fullLink = URL_SHARE_TELEGRAM + window.location.href
+  window.open(fullLink, '_blank').focus();
 })
 
 //share FB
 
 $('.share_social .fb').click(function () {
-    let fullLink = URL_SHAER_FACEBOOK + window.location.href
-    window.open(fullLink, '_blank').focus();
+  let fullLink = URL_SHAER_FACEBOOK + window.location.href
+  window.open(fullLink, '_blank').focus();
 })
